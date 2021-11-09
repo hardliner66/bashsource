77cb38667ed87db3c59d5ae19defdf60fa13e9c3
Steve Biedermann <hardliner66@gmail.com>
2021-11-09 09:57:41 +0100

    allow optional command to be executed by bashsource

--- functions/bashsource.fish
+++ functions/bashsource.fish
@@ -1,6 +1,8 @@
-function bashsource
-	if count $argv > /dev/null
+function bashsource --description "bashsource <file to source> [command to execute]"
+	if test (count $argv) -eq 1
 		bash -c "source $argv && fish"
+	else if test (count $argv) -gt 1
+		bash -c "source $argv[1] && fish -c $argv[2..-1]"
 	else
 		echo "bashsource needs a file to source" >&2
 	end
